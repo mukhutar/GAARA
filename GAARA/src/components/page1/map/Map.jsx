82bcfa866e4cfb121501d93b49d8628f349f1e1c
mukhutar <ngabomukhutar@gmail.com>
2024-01-29 19:26:30 +0200

    new commit

--- GAARA/src/components/page1/map/Map.jsx
+++ GAARA/src/components/page1/map/Map.jsx
@@ -1,35 +1,77 @@
-import React, { useEffect, useRef } from 'react';
-import lottie from 'lottie-web';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion'; // Import motion and AnimatePresence
 import "./map.scss";
-import map from "/assets/map.png"
-import Arrow from '/assets/whiteArrow.svg'
+import FormOne from "./formOne/formOne";
+import map from "/assets/map.png";
+import Arrow from '/assets/whiteArrow.svg';
 
 function Map() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = (e) => {
+    e.preventDefault()
+    
+    setShowPopup(true);
+  }
+
+  const closePopup = () => {
+    setShowPopup(false);
+  }
+
   return (
-    <section className='MapContainer' >
-        <div className="map">
-            <h2>Partner <br /> with <br /> Us</h2>
-            <img src={map} alt="african map" />
-        </div>
+    <section className='MapContainer'>
+      {/* and this animation tooo */}
+      <AnimatePresence>
+        {showPopup && (
+          <motion.div 
+            className="popup-container"
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1 }} 
+            exit={{ opacity: 0 }} 
+            transition={{ duration: 0.3 }} 
+          >
+            <motion.div 
+              className="popup"
+              initial={{ scale: 0.5 }} 
+              animate={{ scale: 1 }} 
+              exit={{ scale: 0.5 }} 
+              transition={{ type: 'spring', stiffness: 300, damping: 25 }} 
+            >
+              <button className="close-btn" onClick={closePopup}>X</button>
+              <FormOne />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    
 
-        <div className="map_texts">
-          <p> <h2> <span>We Cultivate The Change.</span> <span className="spn">Digital Loans 
-             Connect Farmers to Carbon Market <br />  Profits <br /> </span> <br /></h2> <br />  <br />
+    <div className="MapContaine">
+    <div className="map">
+        <h2>Partner <br /> with <br /> Us</h2>
+        <img src={map} alt="african map" />
+      </div>
 
-             <span className='pargraph'>
-             Join GAARA’s forces to revolutionize the agricultural forestry for African smallholder farmers. Our digital tree loan program doesn't just boost harvest volumes it connects farmers to the burgeoning carbon market.
-             </span>
-            </p>
-                 
-           <div className="btn">
-             <button>Let's get started  <img src={Arrow} alt="arrow svg" srcset="" /></button>
+      <div className="map_texts">
+        <p>
+          <h2>
+            <span>We Cultivate The Change.</span>
+            <span className="spn">Digital Loans Connect Farmers to Carbon Market <br />  Profits <br /></span>
+            <br />  <br />
+            <span className='pargraph'>
+              Join GAARA’s forces to revolutionize the agricultural forestry for African smallholder farmers. Our digital tree loan program doesn't just boost harvest volumes it connects farmers to the burgeoning carbon market.
+            </span>
+          </h2>
+          <br /> <br />
+          <div className="btn">
+            <button onClick={openPopup}>Let's get started  <img src={Arrow} alt="arrow svg" srcSet="" /></button>
+          </div>
+        </p>
+      </div>
+    </div>
 
-           </div>
-
-        </div>
       
-    </section >
-  )
+    </section>
+  );
 }
 
-export default Map
+export default Map;
